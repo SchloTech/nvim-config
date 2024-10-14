ae75c6ae8a05c50e4eeff189c7e7572b722a10dc
Jake Schlottag <jschlottag.1gmail.com>
2024-10-14 11:54:10 -0400

    tmux navigator

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -6,3 +6,14 @@ vim.cmd("set number")
 vim.cmd("set relativenumber")
 vim.cmd("set cursorline")
 vim.g.mapleader = " "
+
+vim.opt.swapfile = false
+
+-- Navigate vim panes better
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.wo.number = true
